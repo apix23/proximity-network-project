e8c69b81971fce20e63927c6a31937e8ca8b62f2
mattndross <mattndross@users.noreply.github.com>
2021-12-08 11:12:07 +0100

    updates dbase.sql modifica porque primero se crean las autentificaciones y luego se cargan los datos en las demás tablas

--- back-end/dbase.sql
+++ back-end/dbase.sql
@@ -1,10 +1,18 @@
-drop table if exists stores;
-drop table if exists stores_locations;
-drop table if exists stores_authentications;
-drop table if exists products;
 
+drop table if exists stores_locations;
+drop table if exists products;
+drop table if exists stores;
+drop table if exists stores_authentications;
+
+ create table stores_authentications(
+ 	id						serial primary key,
+ 	store_manager           varchar(50) not null, 
+    manager_email	        varchar(120) not null, 
+    password 		        varchar(100) not null
+    );
 create table stores (
-    id                  serial PRIMARY key,
+    id                  int PRIMARY key,
+ 	foreign key(id)     references stores_authentications(id),
     name                varchar(50) not null,
     store_description   varchar(255), 
     store_category      varchar(50) ,
@@ -14,22 +22,13 @@ create table stores (
     image 			    varchar(255)
 );
 create table stores_locations(
-	store_id		      int,
+	store_id		      int, 
 	address               VARCHAR(120),
     city                  VARCHAR(30),
     postcode              VARCHAR(12),
     country               VARCHAR(20), 
     foreign key(store_id) references stores(id)
  );
-
- create table stores_authentications(
- 	store_id                int primary key, 
- 	foreign key(store_id)   references stores(id),
-    store_manager           varchar(50) not null, 
-    manager_email	        varchar(120) not null, 
-    password 		        varchar(100) not null
-    );
-
 create table products (
     id                    serial primary key,
     store_id              int,
@@ -42,18 +41,20 @@ create table products (
     price                 numeric(4,2), 
     producer              varchar(50), 
     origin                varchar(50),
-    product_image         varchar(100)
+    product_image		  varchar(100)
 );
 
-insert into stores (name, store_description, store_category, web_page,  store_email, phone_number) 
-        values ('Ecoalimentaria', '', 'alimentación' , 'https://www.ecoalimentaria.es/es/', 'eco@mail.com', '632347635');
+insert into stores_authentications ( store_manager, manager_email, password)
+		values ( 'Juan mamon', 'calinga@gmail.com', 'pipo');
+	
+insert into stores (id, name, store_description, store_category, web_page,  store_email, phone_number) 
+        values (1,'Ecoalimentaria', '', 'alimentación' , 'https://www.ecoalimentaria.es/es/', 'eco@mail.com', '632347635');
 
 
 insert into stores_locations (store_id, address, city, postcode, country) 
         values (1, 'chulini 32', 'tarragona', '080231' , 'Spain'); 
    
-insert into stores_authentications (store_id, store_manager, manager_email, password)
-		values (1, 'Juan mamon', 'calinga@gmail.com', 'pipo');   
+   
 
 INSERT INTO products (store_id, product_type, brand, category, product_description, unit, price, producer, origin) 
 		VALUES(1, 'nuez', 'pipon', 'frutos secos', '', '1', '16.10', 'sequia', 'vilaller');   
