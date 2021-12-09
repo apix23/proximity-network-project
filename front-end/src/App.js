8d9629eef2389f4c61ed12ef5770feba2931fd60
Alejandro Jose Sanchez Gomez <sanchezgalejandroj@gmail.com>
2021-12-09 17:24:42 +0100

    nuevo cambio realizado en la app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle"
-import "bootstrap-icons/font/bootstrap-icons.css"
-import "./App";
-import "./index.css"
-import LandingPage from "./pages/LandingPage";
-import StoresList from "./pages/StoresList";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './App';
+import './index.css';
+import LandingPage from './pages/LandingPage';
+import StoresList from './pages/StoresList';
 
 function App() {
   return (
-    <div className="wrapper">
+    <div className='wrapper'>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/stores-list" element={<StoresList />} />
-
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/stores-list' element={<StoresList />} />
         </Routes>
       </BrowserRouter>
     </div>
-  );
+  ); ///
 }
 export default App;
